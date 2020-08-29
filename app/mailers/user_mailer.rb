af99c5ff07c05d392e0d89d2fccc90734d8f3bd5
Johnathan Radünz <johnathan.nunes@outlook.com>
2020-08-29 17:30:14 -0300

    + ADD: User Mailer using SendGrid

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,10 @@
+class UserMailer < ApplicationMailer
+
+    default from: Rails.application.credentials.email_provider_username
+
+    def contact_email(contact)
+        @contact = contact
+        mail(to: Rails.application.credentials.email_provider_username, from: Rails.application.credentials.email_provider_username, :subject => "Website Contact")
+    end
+    
+end
