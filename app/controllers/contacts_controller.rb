9d5c2c57ea385ebe6f57f7258c3d7f4991dccc18
Johnathan Radünz <johnathan.nunes@outlook.com>
2020-08-29 15:40:36 -0300

    Contact Forms v2: The 'Rails way'

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,15 +1,25 @@
 class ContactsController < ApplicationController
-    def process_form
 
-        # Console Log already show the params... 
-        # Rails.logger.debug "DEBUG: params are #{params.inspect}"
-
-        if params[:name].blank?
-            raise 'Name is blank!'
-            flash[:notice] = "Invalid!"
-        end
-
-        flash[:notice] = "Received request from #{params[:name]}"
-        redirect_to root_path
+    def new
+        @contact = Contact.new
     end
+
+    def create
+        @contact = Contact.new(secure_params)
+        if @contact.valid?
+            # TODO send message
+            flash[:notice] = "Message sent from #{@contact.name}."
+            redirect_to root_path
+        else
+            render :new
+        end
+    end
+    
+    private
+    
+    ## WHITE LISTING ACCESSIBLE PARAMS... 
+    def secure_params
+        params.require(:contact).permit(:name, :email, :content)
+    end
+        
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  post 'contact', to: 'contacts#process_form'
+  #post 'contact', to: 'contacts#process_form'
+  
+  ### YOU CAN USE...
+  #get  'contact', to: 'contacts#new'
+  #post 'contact', to: 'contacts#create'
+  ### OR
+  resources :contacts, only: [:new, :create]
+
+
+
   root to: 'visitors#new'
 
 end
